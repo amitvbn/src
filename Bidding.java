40565959ea4cb64ff1289b7cc192d13799bcc38d
amitvbn <amit.iitbhu2010@gmail.com>
2017-01-19 05:13:35 +0530

    improved code

--- Bidding.java
+++ Bidding.java
@@ -1,9 +1,10 @@
 import java.util.*;  
+import java.text.DecimalFormat;
 
 public class Bidding {
 
 	HashMap<Integer,Integer> horses_bids = new HashMap<Integer,Integer>();  
-	HashMap<Integer,Float> won_horses = new HashMap<Integer,Float>(); 
+	LinkedHashMap<Integer,Float> won_horses = new LinkedHashMap<Integer,Float>(); 
 	int percentage;
 	int total_pool=0;
 
@@ -15,7 +16,7 @@ public class Bidding {
 		total_pool += bid;
 		if(horses_bids.containsKey(horse_id)){
 		  bid += horses_bids.get(horse_id);
-		}	  
+		}
 		horses_bids.put(horse_id, bid);
 	}
 
@@ -23,13 +24,17 @@ public class Bidding {
 		won_horses.put(horse_id,(float)horses_bids.get(horse_id));
 	}
 
-	public HashMap<Integer, Float> get_divident(){			
-
-		float pool_per_winner = total_pool *(1-percentage/100)/won_horses.size();
+	public LinkedHashMap<Integer, Float> get_divident(){			
+//		System.out.println(total_pool);
+//		System.out.println(won_horses.size());
+//		System.out.println((1-(float)percentage/100));
+		
+		DecimalFormat df = new DecimalFormat("#.##");
+		float pool_per_winner = total_pool *(1-(float)percentage/100)/won_horses.size();
 		for (Map.Entry<Integer, Float> entry : won_horses.entrySet())
 		{
 			float dividend = pool_per_winner/entry.getValue();
-			won_horses.put(entry.getKey(), dividend);			  
+			won_horses.put(entry.getKey(), Float.parseFloat(df.format(dividend)));			  
 		}			
 		return won_horses;
 	}
--- CopyFile.java
+++ CopyFile.java
@@ -1,32 +1,54 @@
 import java.io.*;
+import java.util.*;
 
 public class CopyFile {
 
+	static	HashMap<String,Bidding> bet_objects = new HashMap<String,Bidding>();  			    
+	
 	   public static void main(String args[]) throws IOException {  
 		   
 		   try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
 			    String line;
+			    Bidding obj1, obj2;
 			    while ((line = br.readLine()) != null) {
 			    	
 			    	String[] split = line.split(":");
 		        	if(split[0].equals("Bet")){
-		        		System.out.println(split[1]) ;
-		        		
-		        		if(split[0].equals("W")){
-		        			
+		    	   		if(split[1].equals("W")){
+		    	   			new_fn(split, "W", 15);
 		        		}
-		        		else if(split[0].equals("P")){
-		        			
+		        		else if(split[1].equals("P")){
+		        			new_fn(split, "P", 12);
 		        		}
 		        	}
 		        	else if(split[0].equals("Result")){
-		        		System.out.println(split[1]) ;		        		
-		        	}
-		        	else{
-		        		
+		        			obj1 = bet_objects.get("W");
+		        			obj1.add_winner(Integer.parseInt(split[1]));
+		        			
+		        			obj2 = bet_objects.get("P");
+		        			obj2.add_winner(Integer.parseInt(split[1]));
+		        			obj2.add_winner(Integer.parseInt(split[2]));
+		        			obj2.add_winner(Integer.parseInt(split[3]));
+		        			
+		        			System.out.println(obj1.get_divident());
+		        			System.out.println(obj2.get_divident());
 		        	}
 			    }
 			    br.close();
 			}
+	   }
+	   
+	   public static void new_fn(String[] split, String bid_type, int percentage){
+		   
+		   		Bidding obj;
+	   			if(bet_objects.containsKey(bid_type)){
+	   				obj = bet_objects.get(bid_type);
+	   			}
+	   			else{
+	   				obj = new Bidding();
+	   				bet_objects.put(bid_type,obj);
+	   				obj.set_percentage(percentage);
+	   			}
+	   			obj.add_bid(Integer.parseInt(split[2]), Integer.parseInt(split[3]));
 	   }
 }
